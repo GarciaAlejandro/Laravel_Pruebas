3e0c1b57015000a0947a9bd33aa7da5ef135fdff
GarciaAlejandro <alejandrogc672@gmail.com>
2019-09-04 00:11:28 -0500

    First commit

--- resources/views/Novia.blade.php
+++ resources/views/Novia.blade.php
@@ -1,16 +1,5 @@
 @extends('plantilla')
 
 @section('seccion')
-<style>
-            html, body {
-                background-color: #000;
-                color: #636b6F;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-</style>
-
     <h1>Fátima del Rosario Uc Lope, Te amo con todo mi corazón<h1>
 @endsection
--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -1,9 +1,23 @@
 <!doctype html>
 <html lang="en">
   <head>
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!--  FONDO NEGRO  -->
+    <style>
+        html, body {
+            background-color: #000;
+            color: #636b6F;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+      </style>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
